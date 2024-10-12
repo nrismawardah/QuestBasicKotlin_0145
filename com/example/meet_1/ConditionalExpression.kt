f90d6bf89d4d9f35346fd5fd1145c0c0e8985d18
nrismawardah <nrismawardah@gmail.com>
2024-10-12 19:57:41 +0700

    Commit 10: Membuat function Perulangan

--- com/example/meet_1/ConditionalExpression.kt
+++ com/example/meet_1/ConditionalExpression.kt
@@ -37,3 +37,29 @@ fun ConditionalStatement() {
         println("Invalid number")
     }
 }
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
