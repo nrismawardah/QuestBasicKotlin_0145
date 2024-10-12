55f0911ccfa7dda577bec0a33509c724471ffcbb
nrismawardah <nrismawardah@gmail.com>
2024-10-12 19:56:59 +0700

    Commit 9: Membuat file ConditionalExpression yang berisi function ConditionalStatement

---
+++ com/example/meet_1/ConditionalExpression.kt
@@ -0,0 +1,39 @@
+package com.example.meet_1
+
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression")
+
+// If-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+// When
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+// Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid number")
+    }
+}
