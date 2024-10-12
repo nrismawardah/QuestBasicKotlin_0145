bcf2176c1a43fc48613e39da57d89dc817c2c6c7
nrismawardah <nrismawardah@gmail.com>
2024-10-12 19:48:07 +0700

    Commit 3: Membuat variable Double, Char, Boolean, dan String

--- com/example/meet_1/TipeData.kt
+++ com/example/meet_1/TipeData.kt
@@ -13,4 +13,23 @@ fun main() {
     val floatValue: Float = 50.6f
     println("Float: $floatValue")
 
+// Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+// Char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+// Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+// String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, World!"
+    println("String: $stringValue")
+
 }
