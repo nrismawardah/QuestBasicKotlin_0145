403954c5ba3ce2692e0f9c54fe31b95c919e1aa7
nrismawardah <nrismawardah@gmail.com>
2024-10-12 19:50:01 +0700

    Commit 4: Membuat file Collection yang berisi function ContohList

---
+++ com/example/meet_1/Collection.kt
@@ -0,0 +1,35 @@
+package com.example.meet_1
+
+// List adalah kumpulan data yang disusun secara berurutan dari
+// List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
+
+fun ContohList() {
+    println("==== List ====")
+
+// List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+// List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+// Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+// Menghapus data dari List Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+// Mengubah data di dalam List Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+// List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
