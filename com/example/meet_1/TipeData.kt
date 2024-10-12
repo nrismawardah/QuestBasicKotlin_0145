3558e75e7bc59c8e0449fece58a04f5acd5de909
nrismawardah <nrismawardah@gmail.com>
2024-10-12 19:45:26 +0700

    Commit 2: Membuat function yang berisi variable Byte, Int, dan Float

--- com/example/meet_1/TipeData.kt
+++ com/example/meet_1/TipeData.kt
@@ -1,1 +1,16 @@
 package com.example.meet_1
+
+fun main() {
+// Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+// Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+// Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+}
