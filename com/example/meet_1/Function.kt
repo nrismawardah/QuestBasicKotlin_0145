0312936942dfb1e4bdad94d901c8dd5c073ae9e5
nrismawardah <nrismawardah@gmail.com>
2024-10-12 20:01:48 +0700

    Commit 13: Membuat file Function yang berisi function withoutParameter dan withParameter

---
+++ com/example/meet_1/Function.kt
@@ -0,0 +1,12 @@
+package com.example.meet_1
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
