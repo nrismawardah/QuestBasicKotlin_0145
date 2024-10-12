72f569c674e190d5dbbf8aae91a20478626102da
nrismawardah <nrismawardah@gmail.com>
2024-10-12 20:04:41 +0700

    Commit 15: Membuat function withReturn dan function yang memanggil function withoutParameter, withParameter, withNamedArgument, dan withDefaultParameter

--- com/example/meet_1/Function.kt
+++ com/example/meet_1/Function.kt
@@ -26,3 +26,14 @@ fun withDefaultParameter(name: String = "Indra", age: Int) {
     println("== withDefaultParameter ==")
     println("Hello, $name! You are $age years old.")
 }
+
+fun withReturn(panjang: Int, lebar: Int): Int {
+    return panjang * lebar
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+}
