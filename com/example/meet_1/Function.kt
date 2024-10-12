a20f28f63f8d53e26b8fc1181bd9777cffd3d99d
nrismawardah <nrismawardah@gmail.com>
2024-10-12 20:02:52 +0700

    Commit 14: Membuat function withNamedArgument dan withDefaultParameter

--- com/example/meet_1/Function.kt
+++ com/example/meet_1/Function.kt
@@ -10,3 +10,19 @@ fun withParameter(name: String) {
     println("== withParameter ==")
     println("Hello, $name!")
 }
+
+// Named argument
+// Named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+// Default parameter value
+// Default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
